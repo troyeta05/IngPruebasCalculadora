12be135b537abd817d54b546d29d1c4fd878e690
troyeta05 <leotroyeta05@gmail.com>
2025-09-23 12:56:13 -0600

    Add files via upload

---
+++ Calculadora V2/CalculadoraV2/src/main/java/pruebas/calculadorav2/CalculadoraV2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pruebas.calculadorav2;
+
+/**
+ *
+ * @author leotr
+ */
+public class CalculadoraV2 {
+
+    public static void main(String[] args) {
+        VistaCalculadora vista = new VistaCalculadora();
+        vista.setVisible(true);
+    }
+}
